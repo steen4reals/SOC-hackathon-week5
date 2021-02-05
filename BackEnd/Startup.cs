3b6dd89536cfdd58ee5c7bbfd3a875878888602c
Stina <christinama92@gmail.com>
2021-02-05 15:42:03 +0000

    cors

--- BackEnd/Startup.cs
+++ BackEnd/Startup.cs
@@ -28,38 +28,37 @@ namespace BackEnd
         {
             services.AddTransient<IRepository<Journal>, JournalRepository>();
             services.AddControllers();
-
-            // Add Cors
-            services.AddCors(o => o.AddPolicy("GetPolicy", builder =>
-            {
-                builder.AllowAnyOrigin()
-                       .AllowAnyMethod()
-                       .AllowAnyHeader();
-            }));
-
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BackEnd", Version = "v1" });
-            });
+            services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(
+                builder =>
+                {
+                    builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+                });
+        });
         }
+        //     services.AddSwaggerGen(c =>
+        //     {
+        //         c.SwaggerDoc("v1", new OpenApiInfo { Title = "BackEnd", Version = "v1" });
+        //     });
+        // }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BackEnd v1"));
-            }
+            // if (env.IsDevelopment())
+            // {
+            //     app.UseDeveloperExceptionPage();
+            //     app.UseSwagger();
+            //     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BackEnd v1"));
+            // }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
             app.UseCors();
 
-            app.UseAuthorization();
+            // app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
@@ -68,3 +67,4 @@ namespace BackEnd
         }
     }
 }
+
