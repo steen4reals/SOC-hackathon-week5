4edaec0ab835b2b53ceb22653f181fa63d2a9eaa
Stina <christinama92@gmail.com>
2021-02-05 12:16:15 +0000

    addtransient

--- BackEnd/Startup.cs
+++ BackEnd/Startup.cs
@@ -26,7 +26,7 @@ namespace BackEnd
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddTransient<IRepository<Journal>, JournalRepository>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
