e3629f980275ad4c7b3d08fba9ac245f256c3f0a
ParadigmZero <josephrcalton@gmail.com>
2021-02-05 16:04:44 +0000

    done

--- FrontEnd/Main.js
+++ FrontEnd/Main.js
@@ -1,14 +1,14 @@
 // // the server backend
 // const BACKEND_URL = "http://localhost:5000";
 
-// // first function to call
+// first function to call
 // async function loadInitialToDos() {
-//     // gets all things from backend
-//     // const res = await fetch(`${BACKEND_URL}/todoitems`);
-//     // Getting all the results as JSON objects
-//     // const data = await res.json();
-//     // rendering each of them ( displaying)
-//     //  data.forEach(renderToDo);
+//     gets all things from backend
+//     const res = await fetch(`${BACKEND_URL}/todoitems`);
+//     Getting all the results as JSON objects
+//     const data = await res.json();
+//     rendering each of them(displaying)
+//     data.forEach(renderToDo);
 // }
 
 
@@ -65,8 +65,11 @@ async function submitEntry(event) {
 
     event.preventDefault();
     const entry = {
-        journalEntry: entryArea.innerText
+        journalEntry: entryArea.value
     }
+
+    entryArea.value = '';
+
     const entryAdd = await postJournalEntry("/journal", "POST", entry);
 
 }
@@ -83,6 +86,34 @@ async function postJournalEntry(path, method, body) {
     return data;
 }
 
+/// not our code below
 
+// async function handleAddToDo(event) {
+//     // stops page reloading by default
+//     event.preventDefault();
+//     // get title and priority from form
+//     const partialTodo = {
+//       title: event.target[0].value,
+//       priority: event.target[1].value,
+//     };
+//     // sent to database and recieve full item including id
+//     const completeTodo = await sendToDo("/todoitems", "POST", partialTodo);
+//     event.target.reset();
+//     renderToDo(completeTodo);
+//   }
 
+//   // sending it off to the database
+//   async function sendToDo(path, method, body = "") {
+//     const res = await fetch(`${BACKEND_URL}${path}`, {
+//       method,
+//       headers: {
+//         "content-type": "application/json",
+//       },
+//       body: JSON.stringify(body),
+//     });
 
+//     // await response (res) from the INSERT
+//     const data = await res.json();
+//     // return response
+//     return data;
+//   }
